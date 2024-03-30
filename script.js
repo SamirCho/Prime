6b6b5176e6ca2b0bc59a04706228a38e2cda31ac
Samir Chokshi <samirchokshi11@gmail.com>
2024-03-30 05:46:41 +0000

    Finished

---
+++ script.js
@@ -0,0 +1,44 @@
+function isPrime(num) {
+    num = parseInt(num);
+    if (isNaN(num)) {
+      return false
+    } else {
+      if(num<0){
+          num*=-1
+      }
+      var isPrime = true;
+        if (num == 1 || num == 0) {
+          return false
+        } else {
+          if (num == 2 || num == 3) {
+            return true
+          } else {
+            for (var i = 2; i < num; i++) {
+              if (num % i == 0) {
+                isPrime = false;
+              }
+            }
+          }
+        }
+      if (
+        isPrime == true &&
+        num != 2 &&
+        num != 1 &&
+        num != 3 &&
+        num != 0 &&
+        num > 0
+      ) {
+            return true
+      } else {
+        if (num > 0 && num != 1 && num != 2 && num != 3) {
+            return false
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < 100000; i++) {
+    if(isPrime(i)){
+        console.log(i)
+    }
+  }
