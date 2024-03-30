98fc2dc7c151cd7b4f95e733ac158a45da0fbd50
Samir Chokshi <samirchokshi11@gmail.com>
2024-03-30 16:13:36 +0000

    Improved prime function

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-function isPrime(num) {
-    num = parseInt(num);
-    if (isNaN(num)) {
-      return false
-    } else {
-      if(num<0){
-          num*=-1
-      }
-      let isPrime = true;
-        if (num == 1 || num == 0) {
-          return false
-        } else {
-          if (num == 2 || num == 3) {
-            return true
-          } else {
-            for (let i = 2; i < num; i++) {
-              if (num % i == 0) {
-                isPrime = false;
-              }
-            }
-          }
-        }
-      if (
-        isPrime == true &&
-        num != 2 &&
-        num != 1 &&
-        num != 3 &&
-        num != 0 &&
-        num > 0
-      ) {
-            return true
-      } else {
-        if (num > 0 && num != 1 && num != 2 && num != 3) {
-            return false
-        }
+function isPrime(num){
+  num=parseInt(num)
+  if(num==0||num==1){
+    return false
+  }
+  if(isNaN(num)){
+    return false
+  }else{
+    for (let i = 2; i < num; i++) {
+      if(num%i==0){
+        return false
       }
     }
+    return true
   }
+}
 
-  for (let i = 0; i < 100000; i++) {
-    if(isPrime(i)){
-        console.log(i)
-    }
+for (let i = 0; i < 1000; i++) {
+  if(isPrime(i)){
+    console.log(i)
   }
+}
